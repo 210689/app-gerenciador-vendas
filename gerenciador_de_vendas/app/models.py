fbc2a006970d9e26876e4b945ba93b9a3ab8835b
210689 <ricardodelima17@gmail.com>
2023-06-27 15:43:32 -0300

    migrating database

--- gerenciador_de_vendas/app/models.py
+++ gerenciador_de_vendas/app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    ProductName = models.CharField(max_length=255)
+    ProductDescription = models.CharField(max_length=255)
+
