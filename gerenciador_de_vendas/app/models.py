b11a2a654d94445815b311c94b1428183e890d6b
210689 <ricardodelima17@gmail.com>
2023-06-27 17:03:53 -0300

    updating product model

--- gerenciador_de_vendas/app/models.py
+++ gerenciador_de_vendas/app/models.py
@@ -3,4 +3,8 @@ from django.db import models
 class Products(models.Model):
     ProductName = models.CharField(max_length=255)
     ProductDescription = models.CharField(max_length=255)
+    ProductPrice = models.FloatField(null=True)
+    ProductStock = models.IntegerField(null=True)
+    OtherDetails = models.CharField(max_length=255, null=True)
+
 
